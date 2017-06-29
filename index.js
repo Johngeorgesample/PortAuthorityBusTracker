625d717b08d21853dec8f990d327d02a9fb172a9
John-George Sample <Johngeorgesample@gmail.com>
2017-06-28 22:12:35 -0400

    Added execute function

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+'use strict';
 var Alexa = require('alexa-sdk');
 
 exports.handler = function(event, context, callback){
     var alexa = Alexa.handler(event, context, callback);
+    alexa.registerHandlers(handlers);
+    alexa.execute();
 };
 
 var handlers = {
